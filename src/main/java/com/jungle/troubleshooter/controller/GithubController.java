5e31f279192540b9dff1a3bbb986e4aedf7e7677
kimyounhoex1 <dordyd11@gmail.com>
2025-06-22 14:40:35 +0900

    feat(pr): pull request 테스트 구현

--- src/main/java/com/jungle/troubleshooter/controller/GithubController.java
+++ src/main/java/com/jungle/troubleshooter/controller/GithubController.java
@@ -1,7 +1,10 @@
 package com.jungle.troubleshooter.controller;
 
 import com.jungle.troubleshooter.service.GithubCommitService;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,31 @@ public class GithubController {
             @RequestParam String repo
     ) {
         return githubCommitService.getCommits(owner, repo);
+    }
+
+    /**
+     * http://localhost:8080/api/github/pullrequest?owner=kimyounhoex1&repo=test
+     * {
+     *     "title":"first pr",
+     *     "head":"pr1",
+     *     "base":"main",
+     *     "body":"my message"
+     * }
+     * @param owner
+     * @param repo
+     * @param requestBody
+     * @return
+     */
+    @PostMapping("/pullrequest")
+    public Mono<String> createPullRequest(
+            @RequestParam String owner,
+            @RequestParam String repo,
+            @RequestBody Map<String, String> requestBody
+    ) {
+        String title = requestBody.get("title");
+        String head = requestBody.get("head");
+        String base = requestBody.get("base");
+        String body = requestBody.get("body");
+        return githubCommitService.createPullRequest(owner, repo, title, head, base, body);
     }
 }
--- src/main/java/com/jungle/troubleshooter/service/GithubCommitService.java
+++ src/main/java/com/jungle/troubleshooter/service/GithubCommitService.java
@@ -1,6 +1,10 @@
 package com.jungle.troubleshooter.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -29,5 +33,37 @@ public class GithubCommitService {
                     .build(owner, repo))
             .retrieve()
             .bodyToMono(String.class); // 또는 CommitDTO[]로 파싱 가능
+    }
+
+    /**
+     *
+     * @param owner : 계정 소유자
+     * @param repo : 레포지토리
+     * @param title : pr name
+     * @param branch : pr 브랜치
+     * @param baseBranch : pr 대상 브랜치
+     * @param prMessage : pr 메세지
+     * @return
+     * 헤더정보를 새로 설정해주는데, DTO를 만들지 않았기에 Map으로 임시 구현
+     * GithubResponseDTO, GithubRequestDTO를 만들어서 추후 코드 수정 필요
+     * Todo: 추후 수정 필요(return 을 Dto 객체로 만들어서 반환해야 함)
+     */
+    public Mono<String> createPullRequest(String owner, String repo,
+                                          String title, String branch,
+                                          String baseBranch, String prMessage) {
+        Map<String, Object> requestBody = Map.of(
+                "title", title,
+                "head", branch,  // <- PR의 출발 브랜치
+                "base", baseBranch,                 // <- PR의 대상 브랜치
+                "body", prMessage
+        );
+
+        return githubWebClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/repos/{owner}/{repo}/pulls")
+                        .build(owner, repo))
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
